90f71f97b506b17e6e7dfc340f29a4f5f0bac1bf
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-03-12 03:37:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+// Created by: Mark Palfi
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("Area and perimeter of a rectangle with dimensions 5 cm and 3 cm");
+        Console.WriteLine("Area = " + (5 * 3));
+        Console.WriteLine("Perimeter = " + ((5 + 3) * 2));
+        Console.WriteLine("\nDone.");
+    }
+}
